a9a5330f04bce18b911f2efa0112677a4dd8c30b
shockingsrose <847073376@qq.com>
2018-01-12 20:07:12 +0800

    完成屎的自动生成、清理    健康值的增加功能

--- assets/Script/Chick.js
+++ assets/Script/Chick.js
@@ -7,32 +7,108 @@
 // Learn life-cycle callbacks:
 //  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
-
-cc.Class({
+var Chick = cc.Class({
     extends: cc.Component,
 
     properties: {
+        shitPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        
+    },
+
+    _parentNode: null,
+    _chickNode: null,
+    // cc.Animation 动画实例
+    _chickAnim: null,
+    //animation_clip 动画剪辑
+    _animMove: null,
+    _animFeed: null,
+    _animTreat: null,
+    _shitCount: null,
+    _shitLabel: null,
+    _shitNode: null,
+    _shitAnim: null,
+    _timer: null,//用于记录屎的生成时间（定时器）
+    //健康值
+    _hpNode: null,
+    _hpLabel: null,
+    _hpBar: null,
+    _hpValue: 0,
        
-    },
-    anim_move: null,
-    anim_feed: null,
-    anim_treat: null,
-
-    init: function(){
-        this.anim_move = this.node.getComponent(cc.Animation)._clips[0];
-        this.anim_feed = this.node.getComponent(cc.Animation)._clips[1];
-        this.anim_treat = this.node.getComponent(cc.Animation)._clips[2];
-        
-    },
+   
+           
+      
     
-    playRun: function(){
+    
+
+
+    init: function () {
+        this._chickNode = this.node;
+        this._chickAnim = this.node.getComponent(cc.Animation);
+        this._animMove = this.node.getComponent(cc.Animation)._clips[0];
+        this._animFeed = this.node.getComponent(cc.Animation)._clips[1];
+        this._animTreat = this.node.getComponent(cc.Animation)._clips[2];
+
+        this._parentNode = cc.find("Canvas");
+        this._shitCount = 0;
+
+        this._hpValue = 20;
+        this._hpNode = this._parentNode.getChildByName('HP');
+        this._hpBar = cc.find("hpBar",this._hpNode).getComponent(cc.ProgressBar);
+        this._hpLabel =cc.find("Value",this._hpNode).getComponent(cc.Label);
+        this.showHp();
         
 
+
+        this.spawnNewShit();
     },
-    onLoad () {
+
+    playChickMove: function () {
+        this._chickAnim.play('click_move');
+
+    },
+    playChickFeed: function () {
+        this._chickAnim.play('click_feed');
+
+    },
+    playChickTreat: function () {
+        this._chickAnim.play('click_treat');
+
+    },
+    spawnNewShit: function () {
+        if (this._parentNode.getChildByName('shit') == null) {
+            this._shitNode = cc.instantiate(this.shitPrefab);
+            this._parentNode.addChild(this._shitNode);
+            this._shitNode.setPosition(cc.p(200, -200));
+            this._shitAnim = this._shitNode.getComponent(cc.Animation);
+            this._shitLabel = this._shitNode.getChildByName('count').getComponent(cc.Label);
+            this._shitNode.active = false;
+        }
+
+    },
+    showShit: function () {
+        if (this._shitCount > 0) {
+            this._shitNode.opacity = 255;
+            this._shitNode.active = true;
+            this._shitLabel.string = "x" + this._shitCount;
+        }
+    },
+    showHp: function(){
+        this._hpLabel.string = "成长值" + this._hpValue + "%";
+        this._hpBar.progress = this._hpValue / 100;
+    },
+    onLoad() {
         this.init();
+        this._timer = setInterval(() => {
+            this._shitCount++;
+        }, 5000);
     },
 
-    
-    // update (dt) {},
+    update(dt) {
+        this.showShit();
+        this.showHp();
+    }
+
 });
--- assets/Script/Index.js
+++ assets/Script/Index.js
@@ -8,52 +8,72 @@
 //  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
 
+//Chick.js 
+
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
+        //Chick 节点 Node
+        Chick: {
+            default: null,
+            type: cc.Node
+        },
        treatIcon: cc.SpriteFrame,
        clearIcon: cc.SpriteFrame,
        feedIcon: cc.SpriteFrame,
     },
+    //Chick.js 
     _chick: null,
-    _chickAnim: null,
+   
     
 
     init: function(){
-        this._chick = this.node.getChildByName("chick01");
-        this._chickAnim = this._chick.getComponent(cc.Animation);
-
-        this._chickAnim.play('chick_move');
+        this._chick = this.Chick.getComponent('Chick');
+        
+        
     },
     //点击治疗事件 弹出alert
     showTreatAlert: function () {
         var self = this;
         Alert.show("此次治疗需要花费***元\n恢复**点成长值",this.treatIcon,function(){
-            //回调 播放动画
-            
-            var animationState = self._chickAnim.play('chick_treat');
-            animationState.repeatCount  = 5;
-           
-
+            self._chick._chickAnim.play('chick_treat');
+            setTimeout(() => {
+                self._chick._chickAnim.play('chick_move');
+            }, 5000);
         });
     },
     //点击清理事件 弹出alert
     showClearAlert: function () {
         var self = this;
         Alert.show("此次清理需要花费***元\n恢复**点成长值",this.clearIcon,function(){
-            self._chickAnim.play('chick_move');
+            self._chick._shitAnim.play('shit');
+            self._chick._shitCount = 0;
+            clearInterval(self._chick._timer);
+            self._chick._timer = setInterval(() => {self._chick._shitCount ++},5000);
         });
     },
     //点击喂食事件 弹出alert
     showFeedAlert: function () {
         var self = this;
         Alert.show("此次喂食需要花费***元\n恢复**点成长值",this.feedIcon,function(){
-            self._chickAnim.play('chick_feed');
+            self._chick._chickAnim.play('chick_feed');
+            setTimeout(() => {
+                self._chick._chickAnim.play('chick_move');
+            }, 5000);
+            //成长值 +1
+            self._chick._hpValue += 1;
         });
+
+        
+    },
+
+    spawnNewShit: function(){
+
     },
     
-    onLoad: function(){
+    start: function(){
         this.init();
     }
 
