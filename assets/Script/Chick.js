b598b4ca2813c195d8f76936c739badad0581199
shockingsrose <847073376@qq.com>
2018-01-17 13:52:31 +0800

    更新第三版 完成 清洁度、健康值、更多按钮 功能

--- assets/Script/Chick.js
+++ assets/Script/Chick.js
@@ -8,110 +8,96 @@
 //  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
 var Chick = cc.Class({
-    name: Chick,
-    extends: cc.Component,
+  name: Chick,
+  extends: cc.Component,
 
-    properties: {
-        shitPrefab: {
-            default: null,
-            type: cc.Prefab
-        },
-        
-    },
-
-    _parentNode: null,
-    _chickNode: null,
-    // cc.Animation 动画实例
-    _chickAnim: null,
-    //animation_clip 动画剪辑
-    _animMove: null,
-    _animFeed: null,
-    _animTreat: null,
-    //屎的属性
-    _shitCount: null,
-    _shitLabel: null,
-    _shitNode: null,
-    _shitAnim: null,
-    _timer: null,//用于记录屎的生成时间（定时器）
-    //健康值
-    _hpNode: null,
-    _hpLabel: null,
-    _hpBar: null,
-    _hpValue: 0,
-       
-   
-           
-      
-    
-    
-
-
-    init: function () {
-        this._chickNode = this.node;
-        this._chickAnim = this.node.getComponent(cc.Animation);
-        this._animMove = this.node.getComponent(cc.Animation)._clips[0];
-        this._animFeed = this.node.getComponent(cc.Animation)._clips[1];
-        this._animTreat = this.node.getComponent(cc.Animation)._clips[2];
-
-        this._parentNode = cc.find("Canvas");
-        this._shitCount = 0;
-
-        this._hpValue = 20;
-        this._hpNode = this._parentNode.getChildByName('HP');
-        this._hpBar = cc.find("hpBar",this._hpNode).getComponent(cc.ProgressBar);
-        this._hpLabel =cc.find("Value",this._hpNode).getComponent(cc.Label);
-        this.showHp();
-        
-
-        this.spawnNewShit();
-    },
-
-    playChickMove: function () {
-        this._chickAnim.play('click_move');
-
-    },
-    playChickFeed: function () {
-        this._chickAnim.play('click_feed');
-
-    },
-    playChickTreat: function () {
-        this._chickAnim.play('click_treat');
-
-    },
-    spawnNewShit: function () {
-        if (this._parentNode.getChildByName('shit') == null) {
-            this._shitNode = cc.instantiate(this.shitPrefab);
-            this._parentNode.addChild(this._shitNode);
-            this._shitNode.setPosition(cc.p(200, -200));
-            this._shitAnim = this._shitNode.getComponent(cc.Animation);
-            this._shitLabel = this._shitNode.getChildByName('count').getComponent(cc.Label);
-            this._shitNode.active = false;
-        }
-
-    },
-    showShit: function () {
-        if (this._shitCount > 0) {
-            this._shitNode.opacity = 255;
-            this._shitNode.active = true;
-            this._shitLabel.string = "x" + this._shitCount;
-        }
-    },
-    showHp: function(){
-        this._hpLabel.string = "成长值" + this._hpValue + "%";
-        this._hpBar.progress = this._hpValue / 100;
-    },
-    onLoad() {
-        this.init();
-        this._timer = setInterval(() => {
-            this._shitCount++;
-        }, 5000);
-    },
-
-    update(dt) {
-        this.showShit();
-        this.showHp();
+  properties: {
+    shitPrefab: {
+      default: null,
+      type: cc.Prefab
     }
+  },
 
+  _parentNode: null,
+  _chickNode: null,
+  // cc.Animation 动画实例
+  _chickAnim: null,
+  //animation_clip 动画剪辑
+  _animMove: null,
+  _animFeed: null,
+  _animTreat: null,
+  //屎的属性
+  _shitCount: null,
+  _shitLabel: null,
+  _shitNode: null,
+  _shitAnim: null,
+  _timer: null, //用于记录屎的生成时间（定时器）
+  //健康值
+  _hpNode: null,
+  _hpLabel: null,
+  _hpBar: null,
+  _hpValue: 0,
+
+  init: function() {
+    this._chickNode = this.node;
+    this._chickAnim = this.node.getComponent(cc.Animation);
+    this._animMove = this.node.getComponent(cc.Animation)._clips[0];
+    this._animFeed = this.node.getComponent(cc.Animation)._clips[1];
+    this._animTreat = this.node.getComponent(cc.Animation)._clips[2];
+
+    this._parentNode = cc.find("Canvas");
+    this._shitCount = 0;
+
+    this._hpValue = 20;
+    this._hpNode = this._parentNode.getChildByName("HP");
+    this._hpBar = cc.find("hpBar", this._hpNode).getComponent(cc.ProgressBar);
+    this._hpLabel = cc.find("Value", this._hpNode).getComponent(cc.Label);
+    this.showHp();
+
+    this.spawnNewShit();
+  },
+
+  playChickMove: function() {
+    this._chickAnim.play("click_move");
+  },
+  playChickFeed: function() {
+    this._chickAnim.play("click_feed");
+  },
+  playChickTreat: function() {
+    this._chickAnim.play("click_treat");
+  },
+  spawnNewShit: function() {
+    if (this._parentNode.getChildByName("shit") == null) {
+      this._shitNode = cc.instantiate(this.shitPrefab);
+      this._parentNode.addChild(this._shitNode);
+      this._shitNode.setPosition(cc.p(200, -200));
+      this._shitAnim = this._shitNode.getComponent(cc.Animation);
+      this._shitLabel = this._shitNode.getChildByName("count").getComponent(cc.Label);
+      this._shitNode.active = false;
+    }
+  },
+  showShit: function() {
+    if (this._shitCount > 0) {
+      this._shitNode.opacity = 255;
+      this._shitNode.active = true;
+      this._shitLabel.string = "x" + this._shitCount;
+    }
+  },
+  showHp: function() {
+    this._hpLabel.string = "成长值" + this._hpValue + "%";
+    this._hpBar.progress = this._hpValue / 100;
+  },
+  onLoad() {
+    this.init();
+    this._timer = setInterval(() => {
+      this._shitCount++;
+    }, 5000);
+  },
+
+  update(dt) {
+    this.showShit();
+    this.showHp();
+  }
 });
 
 var ji = new Chick();
--- assets/Script/Index.js
+++ assets/Script/Index.js
@@ -1,86 +1,122 @@
-// Learn cc.Class:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
-// Learn Attribute:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+//Chick.js
 
-//Chick.js 
-var Chick = require('Chick');
+// 节点不带_   私有变量_
+var Chick = require("Chick");
 
 cc.Class({
-    extends: cc.Component,
- 
-    properties: {
-        //Chick 节点 Node
-        Chick: {
-            default: null,
-            type: cc.Node
-        },
-       treatIcon: cc.SpriteFrame,
-       clearIcon: cc.SpriteFrame,
-       feedIcon: cc.SpriteFrame,
-    },
-    //Chick.js 
-    _chick: null,
-   
-    
+  extends: cc.Component,
 
-    init: function(){
-        this._chick = this.Chick.getComponent('Chick');
-        var chickState = new Chick();
-        
+  properties: {
+    //Chick 节点 Node
+    Chick: {
+      default: null,
+      type: cc.Node
     },
-    //点击治疗事件 弹出alert
-    showTreatAlert: function () {
-        var self = this;
-        Alert.show("此次治疗需要花费***元\n恢复**点成长值",this.treatIcon,function(){
-            self._chick._chickAnim.play('chick_treat');
-            setTimeout(() => {
-                self._chick._chickAnim.play('chick_move');
-            }, 5000);
-        });
+    clearNode: {
+      default: null,
+      type: cc.Node
     },
-    //点击清理事件 弹出alert
-    showClearAlert: function () {
-        var self = this;
-        if(this._chick._shitCount > 0){
-            Alert.show("此次清理需要花费***元\n恢复**点成长值",this.clearIcon,function(){
-                self._chick._shitAnim.play('shit');
-                self._chick._shitCount = 0;
-                clearInterval(self._chick._timer);
-                self._chick._timer = setInterval(() => {self._chick._shitCount ++},5000);
-            });
-        }else{
-            Alert.show("暂时不需要清理",this.clearIcon);
-        }
-       
-    },
-    //点击喂食事件 弹出alert
-    showFeedAlert: function () {
-        var self = this;
-        Alert.show("此次喂食需要花费***元\n恢复**点成长值",this.feedIcon,function(){
-            self._chick._chickAnim.play('chick_feed');
-            setTimeout(() => {
-                self._chick._chickAnim.play('chick_move');
-            }, 5000);
-            //成长值 +1
-            self._chick._hpValue += 1;
-        });
-
-        
-    },
-
-    spawnNewShit: function(){
-
-    },
-    
-    start: function(){
-        this.init();
+    treatIcon: cc.SpriteFrame,
+    clearIcon: cc.SpriteFrame,
+    feedIcon: cc.SpriteFrame,
+    //菜单栏 节点 Node MenuList
+    MenuListNode: {
+      default: null,
+      type: cc.Node
     }
+  },
+  //Chick.js
+  _chick: null,
 
-    // update (dt) {},
+  _clearValue: null,
+  clearLabel: null,
+  clearBar: null,
+  //菜单 半透明背景 Modal_more
+  MenuModal: null,
+
+  init: function() {
+    this._chick = this.Chick.getComponent("Chick");
+    this.clearLabel = this.clearNode.getChildByName("Value").getComponent(cc.Label);
+    this.clearBar = this.clearNode.getChildByName("heath_bar").getComponent(cc.ProgressBar);
+    this.MenuModal = cc.find("Modal_more", this.node);
+    // var chickState = new Chick();
+    this.MenuListNode.active = false;
+
+    this._clearValue = 99;
+    this.clearLabel.string = this._clearValue + "%";
+    this.clearBar.progress = this._clearValue / 100;
+  },
+  //点击治疗事件 弹出alert
+  showTreatAlert: function() {
+    var self = this;
+    Alert.show("此次治疗需要花费***元\n恢复**点成长值", this.treatIcon, function() {
+      self._chick._chickAnim.play("chick_treat");
+      setTimeout(() => {
+        self._chick._chickAnim.play("chick_move");
+      }, 5000);
+    });
+  },
+  //点击清理事件 弹出alert
+  showClearAlert: function() {
+    var self = this;
+    if (this._chick._shitCount > 0) {
+      Alert.show("此次清理需要花费***元\n恢复**点成长值", this.clearIcon, function() {
+        //清洁值 _clearValue
+        self._clearValue =
+          self._clearValue + self._chick._shitCount >= 100 ? 100 : self._clearValue + self._chick._shitCount;
+        self.clearLabel.string = self._clearValue + "%";
+        self.clearBar.progress = self._clearValue / 100;
+
+        self._chick._shitAnim.play("shit");
+        self._chick._shitCount = 0;
+        clearInterval(self._chick._timer);
+        self._chick._timer = setInterval(() => {
+          self._chick._shitCount++;
+        }, 5000);
+      });
+    } else {
+      Alert.show("暂时不需要清理", this.clearIcon);
+    }
+  },
+  //点击喂食事件 弹出alert
+  showFeedAlert: function() {
+    var self = this;
+    Alert.show("此次喂食需要花费***元\n恢复**点成长值", this.feedIcon, function() {
+      self._chick._chickAnim.play("chick_feed");
+      setTimeout(() => {
+        self._chick._chickAnim.play("chick_move");
+      }, 5000);
+
+      //成长值 +1
+      self._chick._hpValue += 1;
+    });
+  },
+  showMenu: function() {
+    var self = this;
+    if (!this.MenuListNode.active) {
+      var fadeIn = cc.fadeIn(0.3);
+      this.MenuModal.runAction(fadeIn);
+      this.MenuListNode.active = !this.MenuListNode.active;
+      var action = cc.moveTo(0.3, cc.p(0, -50));
+
+      this.MenuListNode.runAction(action);
+    } else {
+      var action = cc.sequence(
+        cc.moveTo(0.3, cc.p(0, -800)),
+        cc.callFunc(() => {
+          this.MenuListNode.active = !this.MenuListNode.active;
+        }, this)
+      );
+      this.MenuListNode.runAction(action);
+
+      //菜单栏 半透明背景
+      this.MenuModal.runAction(cc.fadeOut(0.3));
+    }
+  },
+
+  start: function() {
+    this.init();
+  },
+
+  update(dt) {}
 });
