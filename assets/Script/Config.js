77402fb0dd1316dcc6ce48933349d3fc963062f3
chenyomi <408550179@qq.com>
2018-03-02 17:07:49 +0800

    1交易市场筛选切换 完成

---
+++ assets/Script/Config.js
@@ -0,0 +1,5 @@
+window.Config = {
+  backNode: null,
+  backLabel: null,
+  shopP2P: 1
+};
--- assets/Script/ShopP2P.js
+++ assets/Script/ShopP2P.js
@@ -19,10 +19,21 @@ cc.Class({
   goodsListNode: null,
   fillterListNode: null,
   onLoad() {
-    this.fillterButton = cc.find("bg/mygoods", this.node);
-    this.fillterButton.on("click", event => {
+    var self = this;
+    //商品类型 1全部  2我的商品 3鸡蛋 4 贵妃鸡
+    self.fillterClickEvent();
+  },
+  start() {
+    var self = this;
+    self.getList(self.pageIndex, 9, window.Config.shopP2P);
+  },
+  //筛选按钮自定义模态弹框
+  fillterClickEvent() {
+    var self = this;
+    self.fillterButton = cc.find("bg/mygoods", self.node);
+    self.fillterButton.on("click", event => {
       Alert.show("0", null, null, null, null, null, "Prefab/Modal/Shop/filterGoods", function() {
-        var self = this;
+        var selfAlert = this;
         cc.loader.loadRes(Alert._newPrefabUrl, cc.Prefab, function(error, prefab) {
           if (error) {
             cc.error(error);
@@ -30,86 +41,101 @@ cc.Class({
           }
           // 实例
           var alert = cc.instantiate(prefab);
-          // Alert 持有
           Alert._alert = alert;
           //动画
-          self.ready();
-          // 父视图
+          selfAlert.ready();
           Alert._alert.parent = cc.find("Canvas");
-          // 展现 alert
-          self.startFadeIn();
-          self.newButtonEvent(alert, "close");
-          let fillterClick = cc.find("alertBackground/", alert);
+          selfAlert.startFadeIn();
+          selfAlert.newButtonEvent(alert, "close");
+          //绑定四个筛选按钮的筛选事件
+          self.bindClickEvent(cc.find("alertBackground/scrollview/view/content/item1", alert));
+          self.bindClickEvent(cc.find("alertBackground/scrollview/view/content/item2", alert));
+          self.bindClickEvent(cc.find("alertBackground/scrollview/view/content/item3", alert));
+          self.bindClickEvent(cc.find("alertBackground/scrollview/view/content/item4", alert));
         });
       });
     });
   },
-  getList(index, size) {
-    Func.GetGoodList(index, size).then(data => {
-      if (data.List.length == 0) {
-        return (this.hasMore = false);
-      } else {
-        const goodsList = data.List;
-        let clone = cc.instantiate(this.target);
-        clone._name = "page_" + index;
-        let box = cc.find("bg/PageView/view/content", this.node);
-        //获取pageView组件
-        let boxTemp = cc.find("bg/PageView", this.node).getComponent(cc.PageView);
-        //动态添加页面
-        boxTemp.addPage(clone);
-        let goodsListNode = cc.find("page_" + index + "/goodsList", box);
+  //筛选按钮事件绑定
+  bindClickEvent(obj) {
+    var self = this;
+    obj.on("click", function() {
+      window.Config.shopP2P = Number(obj._name.slice(4));
+      cc.director.loadScene("shopP2P");
+    });
+  },
 
-        for (let i = 0; i < goodsList.length; i++) {
-          const goods = goodsList[i];
-          if (!goods.IsDelete) {
-            let goodsNode = cc.instantiate(this.goods_Prefab);
-            // goodsNode.name = goods.PropName;
-            let goodSprite = cc.find("pic-box/pic", goodsNode).getComponent(cc.Sprite);
-            let goodsLabel = cc.find("price-box/goods_label", goodsNode).getComponent(cc.Label);
-            let priceLabel = cc.find("price-box/bg-price/price", goodsNode).getComponent(cc.Label);
-            let count = 1;
-            switch (goods.PropName) {
-              case "鸡蛋":
-                // (function(goodSprite) {
-                //   cc.loader.loadRes("Shop/icon-egg", cc.SpriteFrame, function(err, spriteFrame) {
-                //     goodSprite.spriteFrame = spriteFrame;
-                //   });
-                // })(goodSprite);
-                cc.loader.loadRes("Shop/icon-egg", cc.SpriteFrame, function(err, spriteFrame) {
-                  goodSprite.spriteFrame = spriteFrame;
-                });
-                break;
-              case "饲料":
-                cc.loader.loadRes("Shop/icon-1", cc.SpriteFrame, function(err, spriteFrame) {
-                  goodSprite.spriteFrame = spriteFrame;
-                });
-                count = 10;
-                break;
-            }
-            goodsLabel.string = goods.PropName + "x" + count;
-            priceLabel.string = goods.PropValue * count;
-            goodsListNode.addChild(goodsNode);
-            goodsNode.on("click", event => {
-              Alert.show("是否确认购买该商品？", function() {
-                Func.PostBuy(goods.ID, count).then(data => {
-                  if (data.Code === 1) {
-                    Msg.show("购买成功");
-                  } else {
-                    Msg.show(data.Message);
-                  }
-                });
+  fetchData(index, size, data) {
+    if (data.List.length == 0) {
+      return (this.hasMore = false);
+    } else {
+      const goodsList = data.List;
+      let clone = cc.instantiate(this.target);
+      clone._name = "page_" + index;
+      let box = cc.find("bg/PageView/view/content", this.node);
+      //获取pageView组件
+      let boxTemp = cc.find("bg/PageView", this.node).getComponent(cc.PageView);
+      //动态添加页面
+      boxTemp.addPage(clone);
+      let goodsListNode = cc.find("page_" + index + "/goodsList", box);
+
+      for (let i = 0; i < goodsList.length; i++) {
+        const goods = goodsList[i];
+        if (!goods.IsDelete) {
+          let goodsNode = cc.instantiate(this.goods_Prefab);
+          // goodsNode.name = goods.PropName;
+          let goodSprite = cc.find("pic-box/pic", goodsNode).getComponent(cc.Sprite);
+          let goodsLabel = cc.find("price-box/goods_label", goodsNode).getComponent(cc.Label);
+          let priceLabel = cc.find("price-box/bg-price/price", goodsNode).getComponent(cc.Label);
+          let count = 1;
+          switch (goods.PropName) {
+            case "鸡蛋":
+              // (function(goodSprite) {
+              //   cc.loader.loadRes("Shop/icon-egg", cc.SpriteFrame, function(err, spriteFrame) {
+              //     goodSprite.spriteFrame = spriteFrame;
+              //   });
+              // })(goodSprite);
+              cc.loader.loadRes("Shop/icon-egg", cc.SpriteFrame, function(err, spriteFrame) {
+                goodSprite.spriteFrame = spriteFrame;
+              });
+              break;
+            case "饲料":
+              cc.loader.loadRes("Shop/icon-1", cc.SpriteFrame, function(err, spriteFrame) {
+                goodSprite.spriteFrame = spriteFrame;
+              });
+              count = 10;
+              break;
+          }
+          goodsLabel.string = goods.PropName + "x" + count;
+          priceLabel.string = goods.PropValue * count;
+          goodsListNode.addChild(goodsNode);
+          goodsNode.on("click", event => {
+            Alert.show("是否确认购买该商品？", function() {
+              Func.PostBuy(goods.ID, count).then(data => {
+                if (data.Code === 1) {
+                  Msg.show("购买成功");
+                } else {
+                  Msg.show(data.Message);
+                }
               });
             });
-          }
+          });
         }
-        this.getNextPageList();
       }
+      this.getNextPageList();
+    }
+  },
+  //切换数据接口
+  getList(index, size, e) {
+    var self = this;
+    Func.GetGoodList(index, size).then(data => {
+      self.fetchData(index, size, data);
     });
   },
   getNextPageList() {
     this.pageIndex++;
     if (this.hasMore) {
-      this.getList(this.pageIndex, 9);
+      this.getList(this.pageIndex, 9, window.Config.shopP2P);
     }
   },
   backEvent() {
@@ -117,10 +143,6 @@ cc.Class({
   },
   gotoPage() {
     cc.director.loadScene("shop");
-  },
-  start() {
-    var self = this;
-    self.getList(self.pageIndex, 9);
   }
 
   // update (dt) {},
