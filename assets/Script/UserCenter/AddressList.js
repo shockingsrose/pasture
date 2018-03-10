69d5834037011782a0c67add463cec396decbd01
chenyomi <408550179@qq.com>
2018-03-10 14:27:21 +0800

    3地址列表延时刷新数据

--- assets/Script/UserCenter/AddressList.js
+++ assets/Script/UserCenter/AddressList.js
@@ -7,6 +7,10 @@ cc.Class({
     AddressList_Prefab: {
       default: null,
       type: cc.Prefab
+    },
+    noTip_Prefab: {
+      default: null,
+      type: cc.Prefab
     }
   },
   defaultId: 0,
@@ -18,38 +22,46 @@ cc.Class({
     cc.director.loadScene("AddressAdd");
   },
   getAddressList() {
+    let self = this;
     Data.func.getAddressList().then(data => {
-      for (let i = 0; i < data.List.length; i++) {
-        const PropertyList = cc.instantiate(this.AddressList_Prefab);
+      if (data.List) {
         const PrefabParent = cc.find("scrollview/view/layout/toggleGroup", this.node);
-        let UserName = cc.find("toggle/New Node/label", PropertyList).getComponent(cc.Label);
-        let Address = cc.find("toggle/New Node/label2", PropertyList).getComponent(cc.Label);
-        let box = cc.find("toggle", PropertyList).getComponent(cc.Toggle);
-        let checkButton = cc.find("toggle", PropertyList);
-        let rightButton = cc.find("right", PropertyList);
-        PropertyList._name = "toggleItem_" + data.List[i].ID;
-        console.log(PropertyList._name);
-        if (data.List[i].IsDefault) {
-          self.defaultId = data.List[i].ID;
-          box.isChecked = true;
-          self.check_Prefab = PropertyList;
+        for (let i = 0; i < data.List.length; i++) {
+          const PropertyList = cc.instantiate(this.AddressList_Prefab);
+          let UserName = cc.find("toggle/New Node/label", PropertyList).getComponent(cc.Label);
+          let Address = cc.find("toggle/New Node/label2", PropertyList).getComponent(cc.Label);
+          let box = cc.find("toggle", PropertyList).getComponent(cc.Toggle);
+          let checkButton = cc.find("toggle", PropertyList);
+          let rightButton = cc.find("right", PropertyList);
+          console.log(PropertyList._name);
+          if (data.List[i].IsDefault) {
+            self.defaultId = data.List[i].ID;
+            box.isChecked = true;
+            self.check_Prefab = PropertyList;
+          }
+          box.toggleGroup = PrefabParent.getComponent(cc.ToggleGroup);
+          UserName.string = data.List[i].username + "  " + data.List[i].telNumber;
+          Address.string = data.List[i].addressDetailInfo;
+          checkButton.on("click", function() {
+            self.defaultId = data.List[i].ID;
+            self.check_Prefab = PropertyList;
+          });
+          rightButton.on("click", () => {
+            Config.addressId = data.List[i].ID;
+            cc.director.loadScene("AddressEdit");
+          });
+          PrefabParent.addChild(PropertyList);
         }
-        box.toggleGroup = PrefabParent.getComponent(cc.ToggleGroup);
-        UserName.string = data.List[i].username + "  " + data.List[i].telNumber;
-        Address.string = data.List[i].addressDetailInfo;
-        checkButton.on("click", function() {
-          self.defaultId = data.List[i].ID;
-          self.check_Prefab = PropertyList;
-        });
-        rightButton.on("click", () => {
-          Config.addressId = data.List[i].ID;
-          cc.director.loadScene("AddressEdit");
-        });
+        console.log(self.defaultId);
+      } else {
+        const PropertyList = cc.instantiate(this.noTip_Prefab);
+        const PrefabParent = cc.find("scrollview/view/layout/toggleGroup", this.node);
         PrefabParent.addChild(PropertyList);
       }
     });
   },
   setDefaultId() {
+    let self = this;
     Data.func.setDefaultAddress(self.defaultId).then(data => {
       Msg.show("地址设置成功");
       setTimeout(function() {
@@ -58,13 +70,14 @@ cc.Class({
     });
   },
   delDefaultId() {
-    let item = cc.find("scrollview/view/layout/toggleGroup/toggleItem_" + self.defaultId, this.node);
+    let self = this;
     Data.func.delDefaultAddress(self.defaultId).then(data => {
       Msg.show("地址删除成功");
-      item.destroy();
-      // setTimeout(function() {
-      //   cc.director.loadScene("AddressList");
-      // }, 2000);
+      const PrefabParent = cc.find("scrollview/view/layout/toggleGroup", this.node);
+      setTimeout(function() {
+        PrefabParent.removeAllChildren();
+        self.getAddressList();
+      }, 2000);
     });
   },
   onLoad() {
