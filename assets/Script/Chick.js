05a50b0d45f9341fa2a4a1ee2b52fe5b72ca79b2
shockingsrose <847073376@qq.com>
2018-03-17 14:38:39 +0800

    1.房屋升级完成

--- assets/Script/Chick.js
+++ assets/Script/Chick.js
@@ -286,8 +286,8 @@ var Chick = cc.Class({
     // Msg.show("小鸡饿了，小鸡生病了");
   },
   playChickSickShit: function() {
-    // this._chickAnim.play("chick_shit_sick");
-    this._chickAnim.play("chick_sick");
+    this._chickAnim.play("chick_shit_sick");
+    //this._chickAnim.play("chick_sick");
     // Msg.show("牧场不干净了，小鸡生病了");
   },
   playChickShitHungry: function() {
--- assets/Script/Data.js
+++ assets/Script/Data.js
@@ -359,6 +359,7 @@ var func = {
       xhr.send();
     });
   },
+
   //获得仓库流通物品
   GetRepertoryList() {
     // Loading.show();
@@ -405,6 +406,54 @@ var func = {
       xhr.open("POST", Config.apiUrl + "/T_Base_SignFlow/PostSign", true);
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //缺少这句，后台无法获取参数
       xhr.send("openID=" + this.openID);
+    });
+  },
+  //升级牧场
+  UpgradeHouse(payType) {
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+          if (xhr.status == 200) {
+            var response = xhr.responseText;
+
+            response = JSON.parse(response);
+            resolve(response);
+          } else {
+            var response = xhr.responseText;
+            console.log("签到失败");
+            reject(response);
+          }
+        }
+      };
+      // POST方法
+      xhr.open("POST", Config.apiUrl + "/T_Base_Ranch/PostRanchRankUpgrade", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //缺少这句，后台无法获取参数
+      xhr.send("openID=" + this.openID + "&payType=" + payType);
+    });
+  },
+  //获得牧场升级需要多少钱
+  GetRanchUpGradeMoney() {
+    // Loading.show();
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+          if (xhr.status == 200) {
+            var response = xhr.responseText;
+            response = JSON.parse(response);
+            resolve(response);
+          } else {
+            var response = xhr.responseText;
+            console.log("获取数据失败");
+            reject(response);
+          }
+        }
+      };
+      // GET方法
+      xhr.open("GET", Config.apiUrl + "/T_Base_Ranch/GetRanchUpGradeMoney?openId=" + this.openID, true);
+      xhr.setRequestHeader("Content-Type", "json");
+      xhr.send();
     });
   },
   //牧场清理 type=1 自己清理
