84e0096be472318d89696efc0da32489393670ef
shockingsrose <847073376@qq.com>
2018-03-26 09:33:04 +0800

    Guide 转ES5

--- assets/Script/Guide.js
+++ assets/Script/Guide.js
@@ -1,173 +1,184 @@
+"use strict";
+
 var GuideSystem = {
   step: 0,
   scene: null,
-  guide() {
+  guide: function guide() {
+    var _this = this;
+
     this.scene = cc.find("Canvas");
-    cc.loader.loadRes("Prefab/guide", cc.Prefab, (err, prefab) => {
+    cc.loader.loadRes("Prefab/guide", cc.Prefab, function(err, prefab) {
       if (err) {
         console.log(err);
         return;
       }
-      let oldGuideNode = cc.find("guide", this.scene);
+      var oldGuideNode = cc.find("guide", _this.scene);
       oldGuideNode ? oldGuideNode.removeFromParent() : false;
-      let guideNode = cc.instantiate(prefab);
-      let guideMaskNode = cc.find("mask-guide", guideNode);
-      let modalSprite = cc.find("modal", guideMaskNode).getComponent(cc.Sprite);
-      let circleNode = cc.find("circle", guideMaskNode);
-      switch (this.step) {
+      var guideNode = cc.instantiate(prefab);
+      var guideMaskNode = cc.find("mask-guide", guideNode);
+      var modalSprite = cc.find("modal", guideMaskNode).getComponent(cc.Sprite);
+      var circleNode = cc.find("circle", guideMaskNode);
+      switch (_this.step) {
         case 0:
-          this.guideStep0(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep0(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 1:
-          this.guideStep1(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep1(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 2:
-          this.guideStep2(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep2(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 3:
-          this.guideStep3(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep3(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 4:
-          this.guideStep4(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep4(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 5:
-          this.guideStep5(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep5(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 6:
-          this.guideStep6(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep6(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 7:
-          this.guideStep7(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep7(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
         case 8:
-          this.guideStep8(guideNode, guideMaskNode, modalSprite, circleNode);
+          _this.guideStep8(guideNode, guideMaskNode, modalSprite, circleNode);
           break;
       }
-      this.step++;
-      this.scene.addChild(guideNode, 99);
+      _this.step++;
+      _this.scene.addChild(guideNode, 99);
     });
   },
-  guideStep0(guideNode, guideMaskNode, modalSprite, circleNode) {
-    cc.loader.loadRes("guide/pic", cc.SpriteFrame, (err, spriteFrame) => {
+  guideStep0: function guideStep0(guideNode, guideMaskNode, modalSprite, circleNode) {
+    cc.loader.loadRes("guide/pic", cc.SpriteFrame, function(err, spriteFrame) {
       modalSprite.spriteFrame = spriteFrame;
     });
     circleNode.active = false;
 
     guideNode.on("click", this.guide, this);
   },
-  guideStep1(guideNode, guideMaskNode, modalSprite, circleNode) {
+  guideStep1: function guideStep1(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var _this2 = this;
+
     this.sceneJS = this.scene.getComponent("Index");
     //设置position
-    let btnMoreNode = cc.find("div_menu/more", this.scene);
-    let pos = btnMoreNode.getPosition();
+    var btnMoreNode = cc.find("div_menu/more", this.scene);
+    var pos = btnMoreNode.getPosition();
     //将节点坐标转换成世界坐标系（左下角为原点）
     // let pos_3 = this.btnMoreNode.convertToWorldSpace(pos);
     // let pos_4 = this.btnMoreNode.getNodeToWorldTransform(pos);
-    let pos_5 = btnMoreNode.getNodeToWorldTransformAR(pos);
-    let pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
+    var pos_5 = btnMoreNode.getNodeToWorldTransformAR(pos);
+    var pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
     guideMaskNode.setPosition(pos_6);
 
     // 设置mask宽度和高度
-    let height = btnMoreNode.height;
-    let width = btnMoreNode.width;
-    let radius = height > width ? height : width;
+    var height = btnMoreNode.height;
+    var width = btnMoreNode.width;
+    var radius = height > width ? height : width;
     guideMaskNode.height = radius + 15;
     guideMaskNode.width = radius + 15;
 
-    cc.loader.loadRes("guide/pic-1", cc.SpriteFrame, (err, spriteFrame) => {
+    cc.loader.loadRes("guide/pic-1", cc.SpriteFrame, function(err, spriteFrame) {
       modalSprite.spriteFrame = spriteFrame;
     });
     //绑定事件
     guideMaskNode.once(
       "click",
-      () => {
-        this.sceneJS.func.showMenu.call(this.sceneJS).then(() => {
-          this.guide();
+      function() {
+        _this2.sceneJS.func.showMenu.call(_this2.sceneJS).then(function() {
+          _this2.guide();
         });
       },
       this
     );
   },
-  guideStep2(guideNode, guideMaskNode, modalSprite, circleNode) {
-    let shopNode = cc.find("div_menu/Menu/MenuList/menuScroll/view/content/shop", this.scene);
+  guideStep2: function guideStep2(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var shopNode = cc.find("div_menu/Menu/MenuList/menuScroll/view/content/shop", this.scene);
 
     //设置position
-    let pos = shopNode.getPosition();
-    let pos_5 = shopNode.getNodeToWorldTransformAR(pos);
-    let pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
+    var pos = shopNode.getPosition();
+    var pos_5 = shopNode.getNodeToWorldTransformAR(pos);
+    var pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
     guideMaskNode.setPosition(pos_6);
 
     // 设置mask宽度和高度
-    let height = shopNode.height;
-    let width = shopNode.width;
-    let radius = height > width ? height : width;
+    var height = shopNode.height;
+    var width = shopNode.width;
+    var radius = height > width ? height : width;
     guideMaskNode.height = radius + 15;
     guideMaskNode.width = radius + 15;
-    cc.loader.loadRes("guide/pic-2", cc.SpriteFrame, (err, spriteFrame) => {
+    cc.loader.loadRes("guide/pic-2", cc.SpriteFrame, function(err, spriteFrame) {
       modalSprite.spriteFrame = spriteFrame;
     });
     //绑定事件
     guideMaskNode.on("click", this.sceneJS.func.loadSceneShop, this);
   },
-  guideStep3(guideNode, guideMaskNode, modalSprite, circleNode) {
-    let goodsNode = cc.find("bg/PageView/view/content/page_0/goodsList/goods", this.scene);
+  guideStep3: function guideStep3(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var _this3 = this;
+
+    var goodsNode = cc.find("bg/PageView/view/content/page_0/goodsList/goods", this.scene);
 
     //设置position
-    let pos = goodsNode.getPosition();
-    let pos_5 = goodsNode.getNodeToWorldTransformAR(pos);
-    let pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
+    var pos = goodsNode.getPosition();
+    var pos_5 = goodsNode.getNodeToWorldTransformAR(pos);
+    var pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
     guideMaskNode.setPosition(pos_6);
 
     // 设置mask宽度和高度
-    let height = goodsNode.height;
-    let width = goodsNode.width;
-    let radius = height > width ? height : width;
+    var height = goodsNode.height;
+    var width = goodsNode.width;
+    var radius = height > width ? height : width;
     guideMaskNode.height = radius + 15;
     guideMaskNode.width = radius + 15;
-    cc.loader.loadRes("guide/pic-3-0", cc.SpriteFrame, (err, spriteFrame) => {
+    cc.loader.loadRes("guide/pic-3-0", cc.SpriteFrame, function(err, spriteFrame) {
       modalSprite.spriteFrame = spriteFrame;
     });
     //绑定事件
     guideMaskNode.on(
       "click",
-      () => {
+      function() {
         goodsNode.emit("maskClick");
-        this.guide();
+        _this3.guide();
       },
       this
     );
   },
+
   //确定购买
-  guideStep4(guideNode, guideMaskNode, modalSprite, circleNode) {
+  guideStep4: function guideStep4(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var _this4 = this;
+
     modalSprite.node.active = false;
     circleNode = cc.find("circle", guideMaskNode);
     circleNode.active = false;
-    setTimeout(() => {
-      let SellNode = cc.find("Sell", this.scene);
-      let enterButton = cc.find("bg/btn-group/enterButton", SellNode);
+    setTimeout(function() {
+      var SellNode = cc.find("Sell", _this4.scene);
+      var enterButton = cc.find("bg/btn-group/enterButton", SellNode);
       //设置position
-      let pos = enterButton.getPosition();
-      let pos_5 = enterButton.getNodeToWorldTransformAR(pos);
-      let pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
+      var pos = enterButton.getPosition();
+      var pos_5 = enterButton.getNodeToWorldTransformAR(pos);
+      var pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
       guideMaskNode.setPosition(pos_6);
 
       // 设置mask宽度和高度
-      let height = enterButton.height;
-      let width = enterButton.width;
+      var height = enterButton.height;
+      var width = enterButton.width;
 
       guideMaskNode.height = width;
       guideMaskNode.width = width;
-      let ModalNode = cc.find("Modal", SellNode);
-      let ModalSprite = ModalNode.getComponent(cc.Sprite);
+      var ModalNode = cc.find("Modal", SellNode);
+      var ModalSprite = ModalNode.getComponent(cc.Sprite);
       ModalNode.opacity = 255;
       ModalNode.color = cc.color("#ffffff");
-      cc.loader.loadRes("guide/pic-3", cc.SpriteFrame, (err, spriteFrame) => {
+      cc.loader.loadRes("guide/pic-3", cc.SpriteFrame, function(err, spriteFrame) {
         ModalSprite.spriteFrame = spriteFrame;
       });
 
-      guideMaskNode.on("click", () => {
+      guideMaskNode.on("click", function() {
         //买一个蛋
-        this.PostBuy(8, 1).then(data => {
+        _this4.PostBuy(8, 1).then(function(data) {
           if (data.Code === 1) {
             Msg.show("购买成功");
             setTimeout(function() {
@@ -180,124 +191,133 @@ var GuideSystem = {
       });
     }, 500);
   },
+
   //点击仓库
-  guideStep5(guideNode, guideMaskNode, modalSprite, circleNode) {
+  guideStep5: function guideStep5(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var _this5 = this;
+
     this.sceneJS = this.scene.getComponent("Index");
-    this.sceneJS.func.showMenu.call(this.sceneJS).then(() => {
-      let repertoryNode = cc.find("div_menu/Menu/MenuList/menuScroll/view/content/repertory", this.scene);
+    this.sceneJS.func.showMenu.call(this.sceneJS).then(function() {
+      var repertoryNode = cc.find("div_menu/Menu/MenuList/menuScroll/view/content/repertory", _this5.scene);
 
       //设置position
-      let pos = repertoryNode.getPosition();
-      let pos_5 = repertoryNode.getNodeToWorldTransformAR(pos);
-      let pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
+      var pos = repertoryNode.getPosition();
+      var pos_5 = repertoryNode.getNodeToWorldTransformAR(pos);
+      var pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
       guideMaskNode.setPosition(pos_6);
 
       // 设置mask宽度和高度
-      let height = repertoryNode.height;
-      let width = repertoryNode.width;
-      let radius = height > width ? height : width;
+      var height = repertoryNode.height;
+      var width = repertoryNode.width;
+      var radius = height > width ? height : width;
       guideMaskNode.height = radius + 15;
       guideMaskNode.width = radius + 15;
-      cc.loader.loadRes("guide/pic-4", cc.SpriteFrame, (err, spriteFrame) => {
+      cc.loader.loadRes("guide/pic-4", cc.SpriteFrame, function(err, spriteFrame) {
         modalSprite.spriteFrame = spriteFrame;
       });
       //绑定事件
-      guideMaskNode.on("click", this.sceneJS.func.loadSceneRepertory, this);
+      guideMaskNode.on("click", _this5.sceneJS.func.loadSceneRepertory, _this5);
     });
   },
+
   //点击小鸡
-  guideStep6(guideNode, guideMaskNode, modalSprite, circleNode) {
-    setTimeout(() => {
-      this.sceneJS = this.scene.getComponent("Repertory");
-      let itemNode = cc.find("bg/bg-f3/PageView/view/content/page_1/goodsList/item", this.scene);
+  guideStep6: function guideStep6(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var _this6 = this;
+
+    setTimeout(function() {
+      _this6.sceneJS = _this6.scene.getComponent("Repertory");
+      var itemNode = cc.find("bg/bg-f3/PageView/view/content/page_1/goodsList/item", _this6.scene);
       //设置position
-      let pos = itemNode.getPosition();
-      let pos_5 = itemNode.getNodeToWorldTransformAR(pos);
-      let pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
+      var pos = itemNode.getPosition();
+      var pos_5 = itemNode.getNodeToWorldTransformAR(pos);
+      var pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
       guideMaskNode.setPosition(pos_6);
       // 设置mask宽度和高度
-      let height = itemNode.height;
-      let width = itemNode.width;
-      let radius = height > width ? height : width;
+      var height = itemNode.height;
+      var width = itemNode.width;
+      var radius = height > width ? height : width;
       guideMaskNode.height = radius + 15;
       guideMaskNode.width = radius + 15;
-      cc.loader.loadRes("guide/pic-5", cc.SpriteFrame, (err, spriteFrame) => {
+      cc.loader.loadRes("guide/pic-5", cc.SpriteFrame, function(err, spriteFrame) {
         modalSprite.spriteFrame = spriteFrame;
       });
       //绑定事件
       guideMaskNode.on(
         "click",
-        () => {
+        function() {
           itemNode.emit("maskClick");
-          this.guide();
+          _this6.guide();
         },
-        this
+        _this6
       );
     }, 500);
   },
+
   //孵化小鸡
-  guideStep7(guideNode, guideMaskNode, modalSprite, circleNode) {
+  guideStep7: function guideStep7(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var _this7 = this;
+
     modalSprite.node.active = false;
-    let guideMask = guideMaskNode.getComponent(cc.Mask);
+    var guideMask = guideMaskNode.getComponent(cc.Mask);
     guideMask.type = 0;
     circleNode = cc.find("circle", guideMaskNode);
     circleNode.active = false;
-    setTimeout(() => {
-      let enterButton = cc.find("modal/div/btn-group/btn-red", this.scene);
+    setTimeout(function() {
+      var enterButton = cc.find("modal/div/btn-group/btn-red", _this7.scene);
 
       //设置position
-      let pos = enterButton.getPosition();
-      let pos_5 = enterButton.getNodeToWorldTransformAR(pos);
-      let pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
+      var pos = enterButton.getPosition();
+      var pos_5 = enterButton.getNodeToWorldTransformAR(pos);
+      var pos_6 = guideNode.convertToNodeSpace(cc.v2(pos_5.tx, pos_5.ty));
       guideMaskNode.setPosition(pos_6);
 
       // 设置mask宽度和高度
-      let height = enterButton.height;
-      let width = enterButton.width;
+      var height = enterButton.height;
+      var width = enterButton.width;
 
       guideMaskNode.height = height;
       guideMaskNode.width = width;
 
-      let ModalNode = cc.find("modal/bg", this.scene);
-      let ModalSprite = ModalNode.getComponent(cc.Sprite);
+      var ModalNode = cc.find("modal/bg", _this7.scene);
+      var ModalSprite = ModalNode.getComponent(cc.Sprite);
       ModalNode.opacity = 255;
       ModalNode.color = cc.color("#ffffff");
 
-      cc.loader.loadRes("guide/pic-6", cc.SpriteFrame, (err, spriteFrame) => {
+      cc.loader.loadRes("guide/pic-6", cc.SpriteFrame, function(err, spriteFrame) {
         ModalSprite.spriteFrame = spriteFrame;
       });
 
-      guideMaskNode.on("click", () => {
+      guideMaskNode.on("click", function() {
         enterButton.emit("maskClick");
       });
     }, 500);
   },
+
   //朕已阅
-  guideStep8(guideNode, guideMaskNode, modalSprite, circleNode) {
-    let guideMask = guideMaskNode.getComponent(cc.Mask);
+  guideStep8: function guideStep8(guideNode, guideMaskNode, modalSprite, circleNode) {
+    var guideMask = guideMaskNode.getComponent(cc.Mask);
     guideMask.type = 0;
-    let button = cc.find("btn", guideNode);
+    var button = cc.find("btn", guideNode);
     button.active = true;
     // circleNode = cc.find("circle", guideMaskNode);
     // circleNode.active = false;
 
-    cc.loader.loadRes("guide/pic-7", cc.SpriteFrame, (err, spriteFrame) => {
+    cc.loader.loadRes("guide/pic-7", cc.SpriteFrame, function(err, spriteFrame) {
       modalSprite.spriteFrame = spriteFrame;
     });
 
-    button.on("click", () => {
+    button.on("click", function() {
       Config.firstLogin = false;
       guideNode.removeFromParent();
       Msg.show("恭喜你，完成了新手指引！");
     });
   },
-
-  PostBuy(prId, count) {
+  PostBuy: function PostBuy(prId, count) {
     count = count || 1;
-    return new Promise((resolve, reject) => {
+    return new Promise(function(resolve, reject) {
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+        if (xhr.readyState == 4 && xhr.status >= 200 && xhr.status < 400) {
           if (xhr.status == 200) {
             var response = xhr.responseText;
             console.log("购买成功");
